9f4407592bfd96bcfd906fb155a915ca9a1d2a31
Matthew Holmes <matth@MatthewHo-PC.entelect.local>
2020-12-20 16:00:59 +0200

    Add tests to groups

---
+++ UnitTests/GroupBuilderTests.cs
@@ -0,0 +1,120 @@
+using FluentValidation;
+using Lib.Builders;
+using System;
+using System.Text.Json;
+using Xunit;
+
+namespace UnitTests
+{
+    public class GroupBuilderTests
+    {
+        [Fact]
+        public void GivenAGroupBuilderWithNoUID_ItThrowsValidationException()
+        {
+
+            Assert.Throws<ValidationException>(() => new JSGroupBuilder().Build());
+        }
+
+        [Fact]
+        public void GivenAGroupBuilderWithBlankUID_ItThrowsValidationException()
+        {
+
+            Assert.Throws<ValidationException>(() => new JSGroupBuilder().WithUid("").Build());
+        }
+
+        [Fact]
+        public void GivenAValidGroupBuilder_HasUidInResult()
+        {
+            var result = new JSGroupBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("uid");
+                Assert.Equal("Valid", prop.GetString());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidGroupBuilder_HasTypeInResult()
+        {
+            var result = new JSGroupBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("@type");
+                Assert.Equal("jsgroup", prop.GetString());
+            }
+        }
+        [Fact]
+        public void GivenAValidGroupBuilder_HasEmptyArrayForEntries()
+        {
+            var result = new JSGroupBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("entries");
+                Assert.Equal(0, prop.GetArrayLength());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidGroupBuilderWith1Entries_HasPopulatedArraySize1()
+        {
+            var result = new JSGroupBuilder().WithUid("Valid").WithEvent(e=>e.WithUid("Event1")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("entries");
+                Assert.Equal(1, prop.GetArrayLength());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidGroupBuilderWithTwoOfSameEntries_HasPopulatedArraySize2()
+        {
+            var result = new JSGroupBuilder().WithUid("Valid").WithEvent(e => e.WithUid("Event1")).WithEvent(e=>e.WithUid("Event2")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("entries");
+                Assert.Equal(2, prop.GetArrayLength());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidGroupBuilderWithTwoOfDifferentEntries_HasPopulatedArraySize2()
+        {
+            var result = new JSGroupBuilder().WithUid("Valid").WithEvent(e => e.WithUid("Event1")).WithTask(t=>t.WithUid("Task1")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("entries");
+                Assert.Equal(2, prop.GetArrayLength());
+            }
+        }
+    }
+}
