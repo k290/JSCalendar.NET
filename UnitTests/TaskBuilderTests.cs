376e886bac15ad10b9f80e48304c3351cc4526b8
Matthew Holmes <matth@MatthewHo-PC.entelect.local>
2020-12-20 16:12:40 +0200

    Add TaskBuilder tests

---
+++ UnitTests/TaskBuilderTests.cs
@@ -0,0 +1,58 @@
+using FluentValidation;
+using Lib.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TaskBuilderTests
+    {
+        [Fact]
+        public void GivenATaskBuilderWithNoUID_ItThrowsValidationException()
+        {
+
+            Assert.Throws<ValidationException>(() => new JSTaskBuilder().Build());
+        }
+
+        [Fact]
+        public void GivenATaskBuilderWithBlankUID_ItThrowsValidationException()
+        {
+
+            Assert.Throws<ValidationException>(() => new JSTaskBuilder().WithUid("").Build());
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_HasUidInResult()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("uid");
+                Assert.Equal("Valid", prop.GetString());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_HasTypeInResult()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var prop = rootElement.GetProperty("@type");
+                Assert.Equal("jstask", prop.GetString());
+            }
+        } 
+    }
+}
