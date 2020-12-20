ea5fd3b26dd58fe7ecfe39b7a50c87f20df35f0b
Matthew Holmes <matth@MatthewHo-PC.entelect.local>
2020-12-20 18:04:51 +0200

    Add relatedTo tests to events and tasks

--- UnitTests/Serialization/EventBuilderTests.cs
+++ UnitTests/Serialization/EventBuilderTests.cs
@@ -2,6 +2,7 @@ using FluentValidation;
 using Lib.Builders;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Xunit;
 
@@ -52,6 +53,135 @@ namespace UnitTests.Serialization
                 var rootElement = document.RootElement;
                 var prop = rootElement.GetProperty("@type");
                 Assert.Equal("jsevent", prop.GetString());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidEventBuilderWithoutOptionalRelatedTo_RelatedToNotInResult()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                Assert.Throws<KeyNotFoundException>(() => rootElement.GetProperty("relatedTo"));
+            }
+        }
+
+        [Fact]
+        public void GivenAValidEventBuilder_WithValidOptionalRelatedTo_RelatedToInResult()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var propExists = relatedToProp.TryGetProperty("SomeId", out _);
+                Assert.True(propExists);
+
+            }
+        }
+
+        [Fact]
+        public void GivenAValidEventBuilder_WithMultipleOptionalRelatedTo_BothRelatedToInResult()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).WithRelatedTo("SomeId2", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var subProps = relatedToProp.EnumerateObject();
+                Assert.Equal(2, subProps.Count());
+
+            }
+        }
+
+        [Fact]
+        public void GivenAValidEventBuilder_WithValidOptionalRelatedTo_RelatedToHasType()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var typeProp = relatedToIdProp.GetProperty("@type");
+                Assert.Equal("Relation", typeProp.GetString());
+
+            }
+        }
+
+        [Fact]
+        public void GivenAValidEventBuilder_WithEmptyRelationsInRelatedTo_EmptyObjectInResult()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var relationProp = relatedToIdProp.GetProperty("relation");
+                Assert.Equal(JsonValueKind.Object, relationProp.ValueKind);
+                Assert.Equal(JsonValueKind.Undefined, relationProp.EnumerateObject().Current.Value.ValueKind);
+
+            }
+        }
+
+
+        [Fact]
+        public void GivenAValidEventBuilder_WithRelationsInRelatedTo_RelationsExist()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => r.WithRelation("parent")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var relationProp = relatedToIdProp.GetProperty("relation");
+                var relationTypeProp = relationProp.GetProperty("parent");
+                Assert.True(relationTypeProp.GetBoolean());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidEventBuilder_WithMultipleRelationsInRelatedTo_BothRelationsExist()
+        {
+            var result = new JSEventBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => r.WithRelation("parent").WithRelation("child")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var relationProp = relatedToIdProp.GetProperty("relation");
+                var subProps = relationProp.EnumerateObject();
+                Assert.Equal(2, subProps.Count());
             }
         }
     }
--- UnitTests/Serialization/TaskBuilderTests.cs
+++ UnitTests/Serialization/TaskBuilderTests.cs
@@ -2,6 +2,7 @@ using FluentValidation;
 using Lib.Builders;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Xunit;
 
@@ -53,6 +54,135 @@ namespace UnitTests.Serialization
                 var prop = rootElement.GetProperty("@type");
                 Assert.Equal("jstask", prop.GetString());
             }
-        } 
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilderWithoutOptionalRelatedTo_RelatedToNotInResult()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                Assert.Throws<KeyNotFoundException>(() => rootElement.GetProperty("relatedTo"));
+            }
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_WithValidOptionalRelatedTo_RelatedToInResult()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var propExists = relatedToProp.TryGetProperty("SomeId", out _);
+                Assert.True(propExists);
+
+            }
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_WithMultipleOptionalRelatedTo_BothRelatedToInResult()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).WithRelatedTo("SomeId2", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var subProps = relatedToProp.EnumerateObject();
+                Assert.Equal(2, subProps.Count());
+
+            }
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_WithValidOptionalRelatedTo_RelatedToHasType()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var typeProp = relatedToIdProp.GetProperty("@type");
+                Assert.Equal("Relation", typeProp.GetString());
+
+            }
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_WithEmptyRelationsInRelatedTo_EmptyObjectInResult()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => { }).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var relationProp = relatedToIdProp.GetProperty("relation");
+                Assert.Equal(JsonValueKind.Object, relationProp.ValueKind);
+                Assert.Equal(JsonValueKind.Undefined, relationProp.EnumerateObject().Current.Value.ValueKind);
+
+            }
+        }
+
+
+        [Fact]
+        public void GivenAValidTaskBuilder_WithRelationsInRelatedTo_RelationsExist()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => r.WithRelation("parent")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var relationProp = relatedToIdProp.GetProperty("relation");
+                var relationTypeProp = relationProp.GetProperty("parent");
+                Assert.True(relationTypeProp.GetBoolean());
+            }
+        }
+
+        [Fact]
+        public void GivenAValidTaskBuilder_WithMultipleRelationsInRelatedTo_BothRelationsExist()
+        {
+            var result = new JSTaskBuilder().WithUid("Valid").WithRelatedTo("SomeId", r => r.WithRelation("parent").WithRelation("child")).Build().GetJson();
+            var options = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true
+            };
+            using (var document = JsonDocument.Parse(result, options))
+            {
+                var rootElement = document.RootElement;
+                var relatedToProp = rootElement.GetProperty("relatedTo");
+                var relatedToIdProp = relatedToProp.GetProperty("SomeId");
+                var relationProp = relatedToIdProp.GetProperty("relation");
+                var subProps = relationProp.EnumerateObject();
+                Assert.Equal(2, subProps.Count());
+            }
+        }
     }
 }
