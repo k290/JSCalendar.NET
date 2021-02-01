55aeeb9d3120e15178e70b142c7a4a56cb6d0b50
Matthew <matthewhholmes@rocketmail.com>
2021-02-01 16:56:27 +0200

    The empty string is not a valid setting. A not supplied parameter is always null

--- Lib/Models/JSCalendar/JSGroup.cs
+++ Lib/Models/JSCalendar/JSGroup.cs
@@ -61,7 +61,7 @@ namespace Lib.Models
     {
         public JSGroupValidator()
         {
-            RuleFor(x => x.source).Must(s => Uri.IsWellFormedUriString(s, UriKind.Absolute)).When(x => !string.IsNullOrEmpty(x.source));
+            RuleFor(x => x.source).Must(s => Uri.IsWellFormedUriString(s, UriKind.Absolute)).When(x => x.source != null);
             Include(new JSCommonValidator());
         }
     }
