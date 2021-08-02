fcac638a683c7548893d446b4c23cebc817e4e92
Matthew <matthewhholmes@rocketmail.com>
2021-08-02 21:02:41 +0200

    Add todo

--- Lib/Models/JSCalendar/IGroupEntry.cs
+++ Lib/Models/JSCalendar/IGroupEntry.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Lib.Models
 {
+    //todo removing this attribute and the entries don't appear in client but the tests pass. Add or fix tests
     [JsonInterfaceConverter(typeof(GroupEntryConverter))]
     public interface IGroupEntry
     {
