538d344434de9a49901da8c7622567ef021f25f9
Matthew <matthewhholmes@rocketmail.com>
2021-08-03 16:44:52 +0200

    Reading to do for valueobjects

---
+++ Lib/Models/DataTypes/Utc.cs
@@ -0,0 +1,17 @@
+﻿using Lib.JsonConfiguration;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lib.Models.DataTypes
+{
+
+    //https://github.com/jasontaylordev/CleanArchitecture/blob/main/src/Domain/ValueObjects/Colour.cs
+    //https://codereview.stackexchange.com/questions/169228/standard-primitive-types-vs-custom-structs-in-public-api
+    //https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/user-defined-conversion-operators
+    public class UtcDate
+    {
+
+    }
+}
