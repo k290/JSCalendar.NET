9c990602b29013c85aa614286907ba3b20d99d15
Matthew <matthewhholmes@rocketmail.com>
2021-08-25 18:29:09 +0200

    Rename test project as our goal here is to test behaviours (BDD)

--- Lib/Builders/JSEventBuilder.cs
+++ Lib/Builders/JSEventBuilder.cs
@@ -3,7 +3,7 @@ using Lib.Models;
 using System;
 using System.Text.Json;
 using System.Threading.Tasks;
-
+//todo https://stackoverflow.com/questions/55827354/github-repository-not-listing-in-google-search-no-way-to-submit-url
 namespace Lib.Builders
 {
     public class JSEventBuilder: JSCommonBuilder<JSEvent, JSEventBuilder, JSEventValidator>
